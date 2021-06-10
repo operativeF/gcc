3d4914cbc5215f3ee51cba841e3a01f47c858417
Ben Boeckel <mathstuf@gmail.com>
2021-06-10 13:40:22 -0400

    WIP: p1689r5: initial support
    TODO:
    
      - changelog
      - work-directory support
      - header-unit information fields
      - non-utf8 paths
      - figure out why junk gets placed at the end of the file

--- gcc/cp/module.cc
+++ gcc/cp/module.cc
@@ -19747,7 +19747,7 @@ preprocessed_module (cpp_reader *reader)
 		  && (module->is_interface () || module->is_partition ()))
 		deps_add_module_target (deps, module->get_flatname (),
 					maybe_add_cmi_prefix (module->filename),
-					module->is_header());
+					module->is_header (), module->is_exported ());
 	      else
 		deps_add_module_dep (deps, module->get_flatname ());
 	    }
